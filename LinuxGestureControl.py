e4034d4f17cea624780373e6069d7093ca2c0520
chuptani <yashchothani1324@gmail.com>
2023-12-08 00:01:10 +0530

    routine

---
+++ LinuxGestureControl.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import time
+import HandTrackingModule as htm
+import pyautogui as pag
+
+###############################
+wCam, hCam = 800, 600
+##############################
+
+cap = cv.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(maxHands=1, detectionCon=0.7)
+
+while True:
+    success, img = cap.read()
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    gesture = detector.findGesture(img)
+    img = detector.findHands(img)
+    print(gesture)
+
+    cv.putText(img, str(int(fps)), (10, 60), 
+               cv.FONT_HERSHEY_COMPLEX, 2, (25, 25, 200), 3)
+
+    cv.imshow("Img", img)
+    cv.waitKey(1)
